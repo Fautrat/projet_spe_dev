fe61b5b2a258fca8928f6b2de18a9ee344beeba9
Garnier Andrew <andrewgarnierpro@gmail.com>
2025-07-25 14:06:15 +0200

    Basket route models and controller

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const csurf = require('csurf');
+const helmet = require('helmet');
 require('dotenv').config({ path: '../.env' });
 
 const app = express();
@@ -9,10 +10,12 @@ const PORT = process.env.SERVER_PORT || 3000;
 const productsRoutes = require('./routes/products-routes');
 const authRoutes = require('./routes/auth-routes');
 
+app.use(helmet());
+
 // Can use CORS for just one route if needed
 
 app.use(cors( {
-    origin: 'http://localhost:' + (process.env.VITE_PORT || 5100),
+    origin: 'http://localhost:' + (process.env.VITE_PORT || 5173),
     credentials: true
 }));
 
@@ -23,12 +26,23 @@ app.use(express.urlencoded({extended:false}));
 // Protection csrf
 const csrfProtection = csurf({ cookie: { httpOnly: true, sameSite: 'strict' } });
 app.use(csrfProtection);
+app.get('/api/csrf-token', (req, res) => {
+  const secret = tokens.secretSync();
+  const token = tokens.create(secret);
+
+  res.cookie('csrf-secret', secret, {
+    httpOnly: true,
+    sameSite: 'Strict',
+  });
+
+  res.json({ csrfToken: token });
+});
 
 app.use('/api/products', productsRoutes);
 app.use('/api/auth',authRoutes);
 
 app.get('/api/csrf-token', (req, res) => {
-  res.json({ csrfToken: req.csrfToken() }); 
+  res.json({ csrfToken: req.csrfToken() });
 });
 
 app.listen(PORT, async () => {
--- front/vite.config.js
+++ front/vite.config.js
@@ -5,6 +5,9 @@ config({path: '../.env'});
 export default {
   server: {
     port: process.env.VITE_PORT,
+    headers: {
+      'Content-Security-Policy': "script-src 'self'"
+    },
   },
   build: {
     outDir: 'dist',
